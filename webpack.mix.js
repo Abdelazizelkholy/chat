b790616b5730bfc9938da454456bb3931350fa6d
Ahmed Bltagy <ahmed.bltagy@appssquare.com>
2021-10-08 16:57:09 +0200

    fix webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+mix.setPublicPath('public_html/');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,8 +12,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js('resources/js/app.js', 'js')
+    .postCss('resources/css/app.css', 'css', [
         require('postcss-import'),
         require('tailwindcss'),
     ]);
