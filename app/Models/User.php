a67d1bdffe027e26c57cdc11118539b452e555c9
abdelaziz elkholy <abdelaziz.elkholy@appssquare.com>
2021-11-10 15:38:07 +0200

    New Commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,8 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name',
         'last_name',
-        'phone'
+        'phone',
+        'active',
         'email',
         'password',
     ];
